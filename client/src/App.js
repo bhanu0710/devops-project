91812014826851c3d955a650ab092d45d02124aa
bhanu0710 <brathore0710@gmail.com>
2026-04-22 14:28:49 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080"
+    fetch(`${API_URL}/api/tutorials`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>DevOps 3-Tier App</h1>
+      <h2>Backend Data:</h2>
+
+      {data.length === 0 ? (
+        <p>No data found</p>
+      ) : (
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              <strong>{item.title}</strong> - {item.description}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
